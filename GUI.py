c0de75c6106eb4fd317f1985fe0c909fb5098c38
bheikes-isu <bheikes@iastate.edu>
2025-11-03 19:40:01 -0600

    Started to incorporate OpenAI but did not realize I had to pay. I found a free alternative called Notion. I am going to see if this works effectively. If not I will pay for OpenAI.

--- GUI.py
+++ GUI.py
@@ -11,6 +11,7 @@ weatherdata, hourly_dataframe, current_data= WIP2.getweather(lat,long)
 st.write(f"Current Temperature in {place}: {weatherdata}°F")
 st.write("Suggested outfit is:", WIP2.weather(weatherdata))
 
+#Trying to incorporate weather graph from Plotly into Streamlit
 fig_linechart = px.line(hourly_dataframe, x="hour", y="temperature_2m", markers=True)
 # Update the configuration to enable lasso selection
 fig_linechart.update_layout(dragmode="select", xaxis_title="Time", yaxis_title="Temperature")
